638442610c166c78d9a85401f94c341a11e37d5d
Henry <franzhenry46@gmail.com>
2025-01-28 20:55:18 -0300

    recipient entity

---
+++ src/domain/delivery/entities/Recipient.java
@@ -0,0 +1,7 @@
+package domain.delivery.entities;
+
+public class Recipient extends User{
+    public Recipient(String name, String email, String password){
+        super(name, email, password);
+    }
+}
