0efbd198090be586e3f25746bc0ca3505493ae54
Henry <franzhenry46@gmail.com>
2025-01-28 20:54:37 -0300

    delivery man entity

---
+++ src/domain/delivery/entities/DeliveryMan.java
@@ -0,0 +1,7 @@
+package domain.delivery.entities;
+
+public class DeliveryMan extends User {
+    public DeliveryMan(String name, String email, String password){
+        super(name, email, password);
+    }
+}
