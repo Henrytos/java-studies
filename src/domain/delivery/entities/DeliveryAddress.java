8c749f6dc2ca8007a40b74a114cec53ec3b4f799
Henry <franzhenry46@gmail.com>
2025-01-28 20:54:26 -0300

    delivery address entity

--- src/domain/delivery/entities/DeliveryAddress.java
+++ src/domain/delivery/entities/DeliveryAddress.java
@@ -1,38 +1,32 @@
-public class Address {
+package domain.delivery.entities;
+
+import java.util.UUID;
+
+public class DeliveryAddress {
+    private UUID id = UUID.randomUUID();
     private String state;
     private String city;
     private String road;
     private String number;
 
-    public Address(String state, String city, String road, String number){
+    public DeliveryAddress(String state, String city, String road, String number) {
         this.state = state;
         this.city = city;
         this.road = road;
         this.number = number;
     }
 
-    public String getState(){
-        return this.state;
-    }
 
-    public String getCity() {
-        return this.city;
-    }
-
-    public String getRoad() {
-        return this.road;
-    }
-
-    public String getNumber() {
-        return this.number;
-    }
-
-    public void showData(){
+    public void showData() {
         System.out.println("------- address data -------");
         System.out.println("state: " + this.state);
         System.out.println("city: " + this.city);
         System.out.println("road: " + this.road);
         System.out.println("number: " + this.number);
 
+    }
+
+    public UUID getId() {
+        return this.id;
     }
 }
