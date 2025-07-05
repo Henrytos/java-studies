8528d3a7f69ef8e04695dece6982678ec9a755be
Henry <henry.arcaya@sptech.school>
2025-07-05 14:15:56 -0300

    aula sobre escreita e leitura de arquivos(JAVA IO)

--- testes/src/delivery/src/MainDelivery.java
+++ testes/src/delivery/src/MainDelivery.java
@@ -1,6 +1,8 @@
+package delivery.src;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MainDelivery {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
