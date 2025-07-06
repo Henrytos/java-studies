89658bd585d4c2690da49136f04a8e33fdea053a
Henry <henry.arcaya@sptech.school>
2025-07-06 19:02:18 -0300

    iniciando estudos de spring boot

---
+++ spring-boot/primeiro/src/main/java/com/primeiro_projeto/primeiro/PrimeiroApplication.java
@@ -0,0 +1,13 @@
+package com.primeiro_projeto.primeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroApplication.class, args);
+	}
+
+}
---
+++ spring-boot/primeiro/src/test/java/com/primeiro_projeto/primeiro/PrimeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiro_projeto.primeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
