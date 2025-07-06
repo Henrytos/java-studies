f5d2c3bda675009998ebb0cba04f6a9a058c209a
Henry <henry.arcaya@sptech.school>
2025-07-06 20:18:43 -0300

    QueryParams e RouteParams no SpringBoot

--- spring-boot/primeiro/src/main/java/com/primeiro_projeto/controllers/PrimeiroController.java
+++ spring-boot/primeiro/src/main/java/com/primeiro_projeto/controllers/PrimeiroController.java
@@ -2,7 +2,13 @@ package com.primeiro_projeto.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController()
@@ -13,5 +19,19 @@ public class PrimeiroController {
     public String primeiroMetodo() {
         return "Parabéns henry você conseguiu!";
     }
+
+    @GetMapping("/queryParam")
+    public String queryParam(@RequestParam(name = "id") String id) {
+        return "O valor do parâmetro é(queryParam): " + id;
+    }
     
+    @GetMapping("/routeParam/{id}")
+    public String routeParam(@PathVariable(name = "id") String id) {
+        return "O valor do parâmetro é(routeParam): " + id;
+    }
+
+    @GetMapping("/queryParamAll")
+    public String queryParamAll(@RequestParam Map<String, String> allParams) {
+        return "Todos os parâmetros são: " + allParams.toString(); // allParams.entrySet()
+    }
 }
--- spring-boot/primeiro/src/main/java/com/primeiro_projeto/primeiro/PrimeiroApplication.java
+++ spring-boot/primeiro/src/main/java/com/primeiro_projeto/primeiro/PrimeiroApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.primeiro_projeto.controllers.PrimeiroController;
 
 @SpringBootApplication
 // @ComponentScan(basePackageClasses = PrimeiroController.class)
