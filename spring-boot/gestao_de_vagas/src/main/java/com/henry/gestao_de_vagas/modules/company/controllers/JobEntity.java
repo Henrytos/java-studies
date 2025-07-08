0b110e5a80051862e834db5bbcbde68e78746bc9
Henry <henry.arcaya@sptech.school>
2025-07-08 09:39:56 -0300

    estudando sobre relacionamento @OneToMany em hibernate jakarta.persistence , quando formos referenciar o objeto da classe devemos definir 'name', 'insertable' e 'updatable'

---
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/company/controllers/JobEntity.java
@@ -0,0 +1,41 @@
+package com.henry.gestao_de_vagas.modules.company.controllers;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.henry.gestao_de_vagas.modules.company.entities.CompanyEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "job")
+public class JobEntity {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @ManyToOne()
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    private String benefits;
+    private String description;
+    private String level;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+}
