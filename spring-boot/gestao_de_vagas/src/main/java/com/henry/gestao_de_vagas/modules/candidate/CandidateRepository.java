9b261a1a9178459cfa1aee0665c85ce7cce2200e
Henry <henry.arcaya@sptech.school>
2025-07-08 23:07:09 -0300

    Caso de uso autenticação de candidato

--- spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/candidate/CandidateRepository.java
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/candidate/CandidateRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //@Repository
 // O @Repository é opcional aqui, pois JpaRepository já é um componente do Spring Data, JpaRepository <Entidate, Identificador unico>
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
-    
+
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+
+    Optional<CandidateEntity> findByUsername(String username);
+
 }
---
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,63 @@
+package com.henry.gestao_de_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.henry.gestao_de_vagas.modules.candidate.CandidateRepository;
+import com.henry.gestao_de_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.henry.gestao_de_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO auth) throws AuthenticationException,
+            UsernameNotFoundException {
+        var candidate = this.candidateRepository
+                .findByUsername(auth.username())
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("username/password incorrect");
+                });
+
+        var passwordMatches = this.passwordEncoder.matches(auth.password(), candidate.getPassword());
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+
+        var token = JWT.create()
+                .withIssuer("gestao_de_vagas")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .sign(algorithm);
+
+        AuthCandidateResponseDTO authCandidateResponse = AuthCandidateResponseDTO
+                .builder()
+                .access_token(token)
+                .build();
+
+        return authCandidateResponse;
+    }
+
+}
