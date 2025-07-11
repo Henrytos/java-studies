c71e33e58ff87cf1d5ac9c249ce138eba9dd22ec
Henry <henry.arcaya@sptech.school>
2025-07-10 23:59:22 -0300

    configurando rota inicial com spring security permit requisições para "/couers/"

---
+++ spring-boot/fundamentos/courses_rocket_seat/src/main/java/br/com/course_rocket/api_course_rocket_seat/security/SecurityConfigure.java
@@ -0,0 +1,21 @@
+package br.com.course_rocket.api_course_rocket_seat.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigure {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf((csrf) -> {
+            csrf.disable();
+        }).authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/course/").permitAll();
+        });
+
+        return httpSecurity.build();
+    }
+}
