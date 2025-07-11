d66a13f916efa5f847bdbc807587abafefca60f6
Henry <henry.arcaya@sptech.school>
2025-07-11 19:29:05 -0300

    adiciona validação de campos name e category em CreateCourseRequestDTO e UpdateCourseRequestDTO

--- spring-boot/fundamentos/courses_rocket_seat/src/main/java/br/com/course_rocket/api_course_rocket_seat/modules/course/dto/UpdateCourseRequestDTO.java
+++ spring-boot/fundamentos/courses_rocket_seat/src/main/java/br/com/course_rocket/api_course_rocket_seat/modules/course/dto/UpdateCourseRequestDTO.java
@@ -1,14 +1,22 @@
 package br.com.course_rocket.api_course_rocket_seat.modules.course.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UpdateCourseRequestDTO {
 
+    @NotBlank
     @Pattern(regexp = "^\\S.*\\S$")
+    @Length(min = 3, max = 50)
     private String name;
 
+    @NotBlank
     @Pattern(regexp = "^\\S.*\\S$")
+    @Length(min = 3, max = 50)
     private String category;
+
 }
