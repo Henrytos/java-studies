2ee2cd540a9b6f63bc5b01c4c609c115cb7c56ac
Henry <henry.arcaya@sptech.school>
2025-07-10 23:21:39 -0300

    estrutura basica do projeto do desafio do modulo 3 de spring boot

---
+++ spring-boot/fundamentos/courses_rocket_seat/src/main/java/br/com/courses_rocket/api_courses_rocket_seat/ApiCoursesRocketSeatApplication.java
@@ -0,0 +1,13 @@
+package br.com.courses_rocket.api_courses_rocket_seat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoursesRocketSeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCoursesRocketSeatApplication.class, args);
+	}
+
+}
---
+++ spring-boot/fundamentos/courses_rocket_seat/src/test/java/br/com/courses_rocket/courses_rocket_seat/CoursesRocketSeatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.courses_rocket.courses_rocket_seat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesRocketSeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
