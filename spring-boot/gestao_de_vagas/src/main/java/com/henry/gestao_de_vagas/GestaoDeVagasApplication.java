3876f86f87778ce8b374de5b5dea9a157b9bab0a
Henry <henry.arcaya@sptech.school>
2025-07-12 20:11:45 -0300

    habilitando uso de segurança jwt no swagger

--- spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/GestaoDeVagasApplication.java
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/GestaoDeVagasApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "gestão dez vagas api", description = "api de gestão de vagas do rh", version = "1.0.0"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", in = SecuritySchemeIn.HEADER, description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"")
 public class GestaoDeVagasApplication {
 
 	public static void main(String[] args) {
