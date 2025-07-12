ad54de4e79ba90dc79ded60eb5756accc091b48f
Henry <henry.arcaya@sptech.school>
2025-07-12 20:11:27 -0300

    documentando rota de autenticação de candidato

--- spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/candidate/controllers/AuthCandidateController.java
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -3,8 +3,18 @@ package com.henry.gestao_de_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.henry.gestao_de_vagas.exceptions.ErrorMessageDto;
 import com.henry.gestao_de_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.henry.gestao_de_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
 import com.henry.gestao_de_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +29,14 @@ public class AuthCandidateController {
     private AuthCandidateUseCase authCandidateUseCase;
 
     @PostMapping("/candidate")
+    @Tag(name = "Candidato", description = "funcionalidades de manipulção de candidato")
+    @Operation(summary = "Autenticação de candidatos", description = "Autenticação de candidatos por meio dos campos username e password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = AuthCandidateResponseDTO.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(defaultValue = "username/password incorrect"), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ErrorMessageDto.class)), mediaType = "application/json"))
+    })
     public ResponseEntity<Object> auth(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
-        System.out.println(authCandidateRequestDTO.username());
-        System.out.println(authCandidateRequestDTO.password());
         try {
             var token = this.authCandidateUseCase.execute(authCandidateRequestDTO);
 
@@ -29,7 +44,6 @@ public class AuthCandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-
     }
 
 }
