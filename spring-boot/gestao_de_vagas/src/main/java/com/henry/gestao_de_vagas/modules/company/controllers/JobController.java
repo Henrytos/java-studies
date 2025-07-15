797c01e0182a3e678f5fd9abc5ce7e0dc33f5fbc
Henry <henry.arcaya@sptech.school>
2025-07-14 23:22:57 -0300

    Documentando rota [POST] /company/job/ com Swagger

--- spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/company/controllers/JobController.java
+++ spring-boot/gestao_de_vagas/src/main/java/com/henry/gestao_de_vagas/modules/company/controllers/JobController.java
@@ -8,6 +8,12 @@ import com.henry.gestao_de_vagas.modules.company.dto.CreateJobResponseDTO;
 import com.henry.gestao_de_vagas.modules.company.entities.JobEntity;
 import com.henry.gestao_de_vagas.modules.company.useCases.CreateJobUseCase;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -21,39 +27,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/company/job")
+@Tag(name = "Vagas", description = "Gerenciamento de vagas para companias")
 public class JobController {
 
-    @Autowired
-    private CreateJobUseCase createJobUseCase;
+        @Autowired
+        private CreateJobUseCase createJobUseCase;
 
-    @PostMapping("/")
-    @PreAuthorize("hasRole('COMPANY')")
-    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobRequestDTO createJobDTO,
-            HttpServletRequest request) {
+        @PostMapping("/")
+        @Operation(summary = "Criação de vagas", description = "Criação de vagas para companias na plataforma")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = CreateJobResponseDTO.class), mediaType = "application/json"), description = "Vaga criada com sucesso"),
+                        @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(defaultValue = "Erro ao criar vaga"), mediaType = "application/json"), description = "Erro ao criar vaga"),
+                        @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(defaultValue = "Erro interno do servidor"), mediaType = "application/json"), description = "Erro interno do servidor")
+        })
+        @PreAuthorize("hasRole('COMPANY')")
+        public ResponseEntity<Object> create(@Valid @RequestBody CreateJobRequestDTO createJobDTO,
+                        HttpServletRequest request) {
 
-        var companyId = UUID.fromString(request.getAttribute("company_id").toString());
+                var companyId = UUID.fromString(request.getAttribute("company_id").toString());
 
-        var entity = JobEntity
-                .builder()
-                .description(createJobDTO.getDescription())
-                .level(createJobDTO.getLevel())
-                .benefits(createJobDTO.getBenefits())
-                .companyId(companyId)
-                .build();
+                var entity = JobEntity
+                                .builder()
+                                .description(createJobDTO.getDescription())
+                                .level(createJobDTO.getLevel())
+                                .benefits(createJobDTO.getBenefits())
+                                .companyId(companyId)
+                                .build();
 
-        var result = this.createJobUseCase.execute(entity);
+                var result = this.createJobUseCase.execute(entity);
 
-        var createJobResponseDTO = CreateJobResponseDTO
-                .builder()
-                .id(result.getId())
-                .companyId(companyId)
-                .description(result.getDescription())
-                .level(result.getLevel())
-                .benefits(result.getBenefits())
-                .createdAt(result.getCreatedAt())
-                .build();
+                var createJobResponseDTO = CreateJobResponseDTO
+                                .builder()
+                                .id(result.getId())
+                                .companyId(companyId)
+                                .description(result.getDescription())
+                                .level(result.getLevel())
+                                .benefits(result.getBenefits())
+                                .createdAt(result.getCreatedAt())
+                                .build();
 
-        return ResponseEntity.ok().body(createJobResponseDTO);
-    }
+                return ResponseEntity.ok().body(createJobResponseDTO);
+        }
 
 }
