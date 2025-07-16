fa279a7e64ee3d45536d9ea5126a6d0e80c19854
Henry <henry.arcaya@sptech.school>
2025-07-16 16:23:54 -0300

    ajustes em criando primeiro test de intgração da aplicação,
    estudando um pouco sobre Junit,
    annotations: @Before, ObjcetMapper, WebApplicationContext,MockMvc

--- spring-boot/gestao_de_vagas/src/test/java/com/henry/gestao_de_vagas/modules/company/controllers/JobControllerTest.java
+++ spring-boot/gestao_de_vagas/src/test/java/com/henry/gestao_de_vagas/modules/company/controllers/JobControllerTest.java
@@ -2,8 +2,10 @@ package com.henry.gestao_de_vagas.modules.company.controllers;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -29,7 +31,9 @@ public class JobControllerTest {
 
         var result = mvc.perform(
                 MockMvcRequestBuilders.post("/company/job")
-                        .content(objectToJSON(createJobDTO)));
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectToJSON(createJobDTO)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
         System.out.println(result);
     }
 
