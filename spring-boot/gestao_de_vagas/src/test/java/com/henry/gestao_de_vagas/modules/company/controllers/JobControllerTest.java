9d343cd731cd96a61598035578232e465ac7c7ea
Henry <henry.arcaya@sptech.school>
2025-07-16 19:30:30 -0300

    estudando sobre testes de integração , pelo visto como estamos utilizando 'spring boot security' na nossa aplicação , devemos utilizar um contexto isolado para cada execução http do teste , por isso utilizamos @Before para criar esse setup , atrávez da configuração inicial do nosso mvc (bibiliteca que integra no spring-boot-test= 'MockMvc')

--- spring-boot/gestao_de_vagas/src/test/java/com/henry/gestao_de_vagas/modules/company/controllers/JobControllerTest.java
+++ spring-boot/gestao_de_vagas/src/test/java/com/henry/gestao_de_vagas/modules/company/controllers/JobControllerTest.java
@@ -1,26 +1,40 @@
 package com.henry.gestao_de_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.henry.gestao_de_vagas.modules.company.dto.CreateJobRequestDTO;
+import com.henry.gestao_de_vagas.utils.UtilTest;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class JobControllerTest {
 
     private MockMvc mvc;
 
+    @Autowired
     private WebApplicationContext applicationContext;
 
-    @org.junit.Before
+    @Before
     public void setup() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+        mvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
     }
 
     @Test
@@ -28,19 +42,19 @@ public class JobControllerTest {
     public void should_be_able_a_create_new_job() throws Exception {
         var createJobDTO = CreateJobRequestDTO.builder().description("desenvolvedor java spring boot").level("junior")
                 .benefits("vr,vt,va...").build();
+        var subjectId = UUID.fromString("46f42db8-f91b-4ea2-8949-7be691177a7c");
+        var token = UtilTest.generateToken(subjectId, "COMPANY", "9b3bd2a8-ddbe-4084-afa8-2a75dcbc994c");
 
         var result = mvc.perform(
-                MockMvcRequestBuilders.post("/company/job")
+                MockMvcRequestBuilders.post("/company/job/")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectToJSON(createJobDTO)))
+                        .header("Authorization", "Bearer ".concat(token))
+                        .content(UtilTest.objectToJSON(createJobDTO)
+
+                        )// request
+        )
                 .andExpect(MockMvcResultMatchers.status().isOk());
         System.out.println(result);
-    }
-
-    private static String objectToJSON(Object obj) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        return objectMapper.writeValueAsString(objectMapper);
     }
 
 }
---
+++ spring-boot/gestao_de_vagas/src/test/java/com/henry/gestao_de_vagas/utils/UtilTest.java
@@ -0,0 +1,33 @@
+package com.henry.gestao_de_vagas.utils;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UtilTest {
+    public static String objectToJSON(Object obj) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.writeValueAsString(objectMapper);
+    }
+
+    public static String generateToken(UUID subjectId, String role, String secretKey) {
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expireAt = Instant.now().plus(Duration.ofMinutes(10));
+
+        var token = JWT.create()
+                .withIssuer("gestao_de_vagas")
+                .withSubject(subjectId.toString())
+                .withClaim("roles", Arrays.asList(role))
+                .withExpiresAt(expireAt)
+                .sign(algorithm);
+
+        return token;
+    }
+}
