8dfaa0cd1c9b19c8f964028d267026db68be3df2
Henry <henry.arcaya@sptech.school>
2025-07-19 09:53:15 -0300

    utilizando docker compose para rodar o sonarqube

--- spring-boot/gestao_de_vagas/script.sh
+++ spring-boot/gestao_de_vagas/script.sh
@@ -1,5 +1,9 @@
+Analyze "gestao_vagas": sqp_8fa3411ec07439b04518e62d48d829ae2499c491
 
 
 sudo docker run -d -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:9.9.0-community
 
-mvn clean verify sonar:sonar   -Dsonar.projectKey=gestao_vagas   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=sqp_fb0953d7d200963e13b2d2b803c1f0d9f9f5c006
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=gestao_vagas \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=sqp_8fa3411ec07439b04518e62d48d829ae2499c491
