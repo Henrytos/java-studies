5ec92c96605943e6a4d980725320467138ff6cc0
Henry <henry.arcaya@sptech.school>
2025-08-01 22:51:05 -0300

    feat: add .dockerignore file to exclude unnecessary files from Docker context

--- spring-boot/devlog/src/test/java/com/log/dev/api/modules/user/controllers/CreateUserControllerTest.java
+++ spring-boot/devlog/src/test/java/com/log/dev/api/modules/user/controllers/CreateUserControllerTest.java
@@ -1,6 +1,5 @@
 package com.log.dev.api.modules.user.controllers;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
