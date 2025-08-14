53f186c0209520baf5872948d5194aeb17e69b9a
Henry <henry.arcaya@sptech.school>
2025-08-13 23:57:07 -0300

    criando pasta sptecj

--- sptech/projeto-nivelamento/src/test/java/school/sptech/Exemplo07.java
+++ sptech/projeto-nivelamento/src/test/java/school/sptech/Exemplo07.java
@@ -5,13 +5,13 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Exemplo07 {
     public static void main(String[] args) {
         // Math.floor() * 11 + 10
-        Integer numero = ThreadLocalRandom.current().nextInt(10,21);
+        Integer numero = ThreadLocalRandom.current().nextInt(10, 21);
         System.out.println(numero.toString());
 
-        Double numero2 = ThreadLocalRandom.current().nextDouble(10,21);
+        Double numero2 = ThreadLocalRandom.current().nextDouble(10, 21);
         System.out.println(numero2.toString());
 
-        Boolean b= ThreadLocalRandom.current().nextBoolean();
+        Boolean b = ThreadLocalRandom.current().nextBoolean();
         System.out.println(b);
     }
 }
