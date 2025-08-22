a00da0a70a4f21d91af07090c4ebd92bec2ddb2e
henry <henry.arcaya@sptech.school>
2025-08-22 16:07:44 -0300

    vendo sobre metho reference

--- avancados/src/programacao_funcional/Main.java
+++ avancados/src/programacao_funcional/Main.java
@@ -3,19 +3,24 @@ package programacao_funcional;
 public class Main {
 
     // INNER CLASSES
-    static SaudacaoFuncaoInterface saudacaoFuncaoInterfaceInnerClasse = new SaudacaoFuncaoInterface() {
+    static final SaudacaoFuncaoInterface saudacaoFuncaoInterfaceInnerClasse = new SaudacaoFuncaoInterface() {
         @Override
         public void saudar() {
             System.out.println("olá função saudar inner classe");
         }
     };
 
-    static SaudacaoFuncaoInterface saudacaoFuncaoInterfaceLambda = () -> System.out.println("olá função lambda saudar");
+    static final SaudacaoFuncaoInterface saudacaoFuncaoInterfaceLambda = () -> System.out
+            .println("olá função lambda saudar");
+
+    static final ProcessString processString = System.out::println;
 
     public static void main(String[] args) {
         saudacaoFuncaoInterfaceInnerClasse.saudar();
         saudacaoFuncaoInterfaceLambda.saudar();
         Runnable runnable = () -> System.out.println("olá função saudar em runner");
         runnable.run();
+
+        processString.process("olá função method reference");
     }
 }
---
+++ avancados/src/programacao_funcional/ProcessString.java
@@ -0,0 +1,9 @@
+
+package programacao_funcional;
+
+@FunctionalInterface
+public interface ProcessString {
+
+    void process(String str);
+
+}
