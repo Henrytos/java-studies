e8adfd83b70202eca75e89d0eaab3606557af1f1
henry <henry.arcaya@sptech.school>
2025-08-22 16:01:20 -0300

    estudando um pouco sobre lambdas

--- avancados/src/programacao_funcional/Main.java
+++ avancados/src/programacao_funcional/Main.java
@@ -1,14 +1,21 @@
 package programacao_funcional;
 
 public class Main {
-    static SaudacaoFuncaoInterface saudacaoFuncaoInterface = new SaudacaoFuncaoInterface() {
+
+    // INNER CLASSES
+    static SaudacaoFuncaoInterface saudacaoFuncaoInterfaceInnerClasse = new SaudacaoFuncaoInterface() {
         @Override
         public void saudar() {
-            System.out.println("olá função saudar");
+            System.out.println("olá função saudar inner classe");
         }
     };
 
+    static SaudacaoFuncaoInterface saudacaoFuncaoInterfaceLambda = () -> System.out.println("olá função lambda saudar");
+
     public static void main(String[] args) {
-        saudacaoFuncaoInterface.saudar();
+        saudacaoFuncaoInterfaceInnerClasse.saudar();
+        saudacaoFuncaoInterfaceLambda.saudar();
+        Runnable runnable = () -> System.out.println("olá função saudar em runner");
+        runnable.run();
     }
 }
