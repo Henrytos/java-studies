271075ca7d3b241fca08b5941d083ad66be48d90
henry <henry.arcaya@sptech.school>
2025-09-10 12:22:06 -0300

    iniciando estudos praticos com arquitetura hexagonal e ddd

---
+++ hexagonal-ddd/src/main/java/com/estudo/hexagonal_ddd/HexagonalDddApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.hexagonal_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalDddApplication.class, args);
+	}
+
+}
---
+++ hexagonal-ddd/src/test/java/com/estudo/hexagonal_ddd/HexagonalDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.hexagonal_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
