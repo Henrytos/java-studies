5a01c9eaaf5ab8defadb3db6fbd3f81f2047a14a
henry <henry.arcaya@sptech.school>
2025-09-17 13:44:05 -0300

    iniciando estudo de flyway e jpa

--- hexagonal-ddd/src/main/java/com/estudo/hexagonal_ddd/infrastructure/configuration/services/UserServiceConfiguration.java
+++ hexagonal-ddd/src/main/java/com/estudo/hexagonal_ddd/infrastructure/configuration/services/UserServiceConfiguration.java
@@ -11,23 +11,23 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserServiceConfiguration {
-
-    @Bean
-    public UserServicePort userService(
-            JPAUserRepositoryImpl jpaUserRepository,
-            HashEncoder hashEncoder,
-            TokenService tokenService
-    ){
-        return  new UserServiceImpl(
-                jpaUserRepository,
-                hashEncoder,
-                tokenService
-        );
-    }
-
-    @Bean
-    public  UserRepository userRepository(){
-        return new JPAUserRepositoryImpl();
-    }
+//
+//    @Bean
+//    public UserServicePort userService(
+//            JPAUserRepositoryImpl jpaUserRepository,
+//            HashEncoder hashEncoder,
+//            TokenService tokenService
+//    ){
+//        return  new UserServiceImpl(
+//                jpaUserRepository,
+//                hashEncoder,
+//                tokenService
+//        );
+//    }
+//
+//    @Bean
+//    public  UserRepository userRepository(){
+//        return new JPAUserRepositoryImpl();
+//    }
 
 }
