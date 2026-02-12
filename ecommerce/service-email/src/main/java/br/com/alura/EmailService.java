299e7022b1f0885fd95ace72bcddf96cfdbca45b
Henrytos <henry.arcaya@sptech.school>
2026-02-12 12:23:14 -0300

    criando submodulos para utilizacao de microservices

--- ecommerce/service-email/src/main/java/br/com/alura/EmailService.java
+++ ecommerce/service-email/src/main/java/br/com/alura/EmailService.java
@@ -9,7 +9,7 @@ public class EmailService {
     public static void main(String[] args) {
         EmailService emailService = new EmailService();
 
-        try (var kafkaService = new KafkaService<Email>(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL_DEV", emailService::parse, Email.class, Map.of())) {
+        try (var kafkaService = new KafkaService<>(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL_DEV", emailService::parse, Email.class, Map.of())) {
             kafkaService.run();
         }
     }
--- ecommerce/service-log/src/main/java/br/com/alura/LogService.java
+++ ecommerce/service-log/src/main/java/br/com/alura/LogService.java
@@ -5,7 +5,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
