ed51e7abf73b93dd40d62fc1d2f9695b31b7a861
Henrytos <henry.arcaya@sptech.school>
2026-02-13 14:32:15 -0300

    adicionando api para facilitar a distribuicao de mensagens de pedido

---
+++ ecommerce/service-http-ecommerce/src/main/java/br/com/alura/NewOrderServlet.java
@@ -0,0 +1,43 @@
+package br.com.alura;
+
+import org.eclipse.jetty.servlet.Source;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class NewOrderServlet extends HttpServlet {
+    private final KafkaDispatch<Order> orderDispatch = new KafkaDispatch<>();
+    private final KafkaDispatch<Email> emailDispatch = new KafkaDispatch<>();
+
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatch.close();
+        emailDispatch.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var orderId = UUID.randomUUID().toString();
+        var subject = req.getParameter("email");
+        var amount = new BigDecimal(req.getParameter("amount"));
+
+        try {
+            var order = new Order(orderId, amount, subject);
+            orderDispatch.send("ECOMMERCE_NEW_ORDER_DEV", subject, order);
+
+            var email = new Email(subject, "thank you for you order!");
+            emailDispatch.send("ECOMMERCE_SEND_EMAIL_DEV", subject, email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
